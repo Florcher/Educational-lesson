df7267e15bbcc037ec2ae685b90fa27f6c112cf3
Florch <roman-kulik-87@inbox.ru>
2024-06-06 18:50:56 +0300

    little rework

--- Project13/Drawer.h
+++ Project13/Drawer.h
@@ -9,7 +9,7 @@
 class PROJECT_EXPORT AbstractDrawer {
 public:
 	virtual void drawLine(const Math::LineSegment2D& line) = 0;
-	using ptr = std::shared_ptr<Drawer>;
+	using ptr = std::shared_ptr<AbstractDrawer>;
 };
 
 class PROJECT_EXPORT Drawer : public AbstractDrawer {
